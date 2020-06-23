85119c06dd4a17355c0cf5621b05d583bf9c17f5
Yuriy Zasorin <agafonia3010@gmail.com>
2020-06-23 16:55:26 +0400

    Добавил Navigation component Создал структуру проекта

--- app/src/main/java/ru/teamdroid/colibripost/MainActivity.kt
+++ app/src/main/java/ru/teamdroid/colibripost/MainActivity.kt
@@ -2,10 +2,27 @@ package ru.teamdroid.colibripost
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import ru.teamdroid.colibripost.presentation.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeFragment(MainFragment(), MainFragment.TAG)
+    }
+
+    private fun changeFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragment).addToBackStack(tag)
+        }.commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 1)
+            supportFragmentManager.popBackStack()
+        else
+            finish()
     }
 }
